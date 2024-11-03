faae076d506ef87a59164889413232c4550e28f8
arm-out <arminsuraj@gmail.com>
2024-11-03 22:31:47 +0800

    refactor: modularize menubar

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,15 @@
+mod menu;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![])
+        .setup(|app| {
+            // menu::create_menu(app)?;
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src-tauri/src/menu.rs
@@ -0,0 +1,18 @@
+use tauri::Runtime;
+
+pub fn create_menu<R: Runtime>(app: &tauri::App<R>) -> tauri::Result<()> {
+
+    // let file_menu = SubmenuBuilder::new(app, "File")
+    //             .text("open", "open")
+    //             .text("quit", "quit")
+    //             .build()?;
+
+    // let menu = MenuBuilder::new(app)
+    //     .items(&[&file_menu])
+    //     .build()?;
+
+    // app.set_menu(menu)?;
+
+    Ok(())
+
+}
