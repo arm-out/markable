2b86574b637495530e746c000015871e0a6a64b6
arm-out <arminsuraj@gmail.com>
2024-11-06 00:40:46 +0800

    feat: init editor and open file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,56 @@
 <script lang="ts">
 	import { invoke } from '@tauri-apps/api/core';
+	import { onDestroy } from 'svelte';
+	import { Editor } from '@tiptap/core';
+	import StarterKit from '@tiptap/starter-kit';
 
-	let data = '';
+	let content = $state('');
+	let editorElement: HTMLDivElement;
+	let editor: Editor;
+
+	$effect(() => {
+		if (editor) {
+			editor.destroy();
+		}
+
+		editor = new Editor({
+			element: editorElement,
+			extensions: [StarterKit],
+			content,
+			onTransaction: () => {
+				editor = editor;
+			}
+		});
+	});
+
+	onDestroy(() => {
+		if (editor) {
+			editor.destroy();
+		}
+	});
+
+	$inspect(content);
 
 	async function open() {
 		invoke('open_file').then((res) => {
 			const buffer = new Uint8Array(res as ArrayBuffer);
-			data = String.fromCharCode(...buffer);
+			content = String.fromCharCode(...buffer);
 		});
 	}
 </script>
 
-<button on:click={open}>Open</button>
-<div>{data}</div>
+<div style:min-height="100vh">
+	<button on:click={open}>Open</button>
+	<button>Save As</button>
+	<button>Save</button>
+
+	<!-- svelte-ignore element_invalid_self_closing_tag -->
+	<div bind:this={editorElement} class="editor" />
+</div>
+
+<style>
+	:global(.tiptap) {
+		height: 100vh;
+		outline: none;
+	}
+</style>
