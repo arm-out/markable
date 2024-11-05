b13895534b0975fa28a59d61c7fea5dc8fcb17c1
arm-out <arminsuraj@gmail.com>
2024-11-06 01:28:45 +0800

    build: comrak dep

--- src-tauri/src/fs.rs
+++ src-tauri/src/fs.rs
@@ -7,8 +7,13 @@ use tauri_plugin_fs::FsExt;
 pub async fn open_file(app: tauri::AppHandle) -> Response {
     let file_path = app.dialog().file().add_filter("markdown", &["md"]).blocking_pick_file();
     if let Some(file_path) = file_path {
-        let content = app.fs().read(file_path).unwrap();
-        return tauri::ipc::Response::new(content)
+        let content = app.fs().read_to_string(file_path).unwrap();
+        let md = parse_md(content);
+        return tauri::ipc::Response::new(md)
     }
     tauri::ipc::Response::new("Error opening file".to_string())
+}
+
+fn parse_md(content: String) -> Vec<u8> {
+    content.as_bytes().to_vec() 
 }
