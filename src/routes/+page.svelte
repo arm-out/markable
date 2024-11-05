1435e0f1f29de11c944976fcc0c2e4fee5cc5e0b
arm-out <arminsuraj@gmail.com>
2024-11-06 01:25:09 +0800

    refactor: update onclick to svelte 5 variant

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,13 +34,14 @@
 	async function open() {
 		invoke('open_file').then((res) => {
 			const buffer = new Uint8Array(res as ArrayBuffer);
+			console.log(buffer);
 			content = String.fromCharCode(...buffer);
 		});
 	}
 </script>
 
 <div style:min-height="100vh">
-	<button on:click={open}>Open</button>
+	<button onclick={open}>Open</button>
 	<button>Save As</button>
 	<button>Save</button>
 
