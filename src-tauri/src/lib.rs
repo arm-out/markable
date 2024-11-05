34f467512271fc6731036c2aea6ac9d84c28fadc
arm-out <arminsuraj@gmail.com>
2024-11-06 02:30:50 +0800

    feat: open devtools in dev mode

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,8 @@
 mod fs;
 mod menu;
 
+use tauri::Manager;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
@@ -10,6 +12,10 @@ pub fn run() {
         .invoke_handler(tauri::generate_handler![fs::open_file])
         .setup(|app| {
             // menu::create_menu(app)?;
+            #[cfg(dev)] {
+                let window = app.get_webview_window("main").unwrap();
+                window.open_devtools();
+            }
             Ok(())
         })
         .run(tauri::generate_context!())
